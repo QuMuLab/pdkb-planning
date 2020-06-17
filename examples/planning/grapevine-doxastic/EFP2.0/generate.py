b5999b6d95786aa4ed652ca8da76be64f5dda87e
Christian Muise <christian.muise@gmail.com>
2020-06-17 00:17:43 -0400

    Start of the EFP version of grapevine.

---
+++ examples/planning/grapevine-doxastic/EFP2.0/generate.py
@@ -0,0 +1,13 @@
+
+import sys
+
+if len(sys.argv) != 4:
+    print("Usage: python generate.py <num agents> <num goals> <depth>")
+    exit(0)
+
+# Coming soon...
+
+# TODO
+# - lift / store each of the individual goals
+# - generate two forms (4ag / 8ag)
+# - combine the domain+goal and output
